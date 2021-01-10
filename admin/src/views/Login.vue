f2dd608ecfa2c12c746fb7e52b8683d6f5e5c49d
agumr <agumr@qq.com>
2021-01-10 15:11:58 +0800

    init project

---
+++ admin/src/views/Login.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="login">
+    <canvas ref="cvsRef"></canvas>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  mounted() {
+    const cvs = this.$refs.cvsRef;
+    console.log(cvs);
+  }
+};
+</script>
---
+++ admin/vue.config.js
@@ -0,0 +1,11 @@
+const Timestamp = new Date().getTime();
+
+module.exports = {
+  transpileDependencies: ["vuetify"],
+  configureWebpack: {
+    output: {
+      filename: `[name].[hash].js`,
+      chunkFilename: `[name].[hash].js`
+    }
+  },
+};
